1c25562ad706bf3c2c10419fb51b0cbf0f1c9f00
Darin Ma <darinmma@gmail.com>
2016-10-27 12:35:01 -0700

    adds edit, update and destroy to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -26,6 +26,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    if @user != User.find(session[:user_id])
+      redirect_to users_path
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to new_user_path
+  end
 private
 
   def user_params
